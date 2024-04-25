cde6acdef6753159ed62ea2b1fce84f1ffdef7a7
ABU <muruvat@gmail.com>
2024-04-25 22:22:55 +0500

    new for html

--- shop/views.py
+++ shop/views.py
@@ -35,71 +35,19 @@ class ContactPageView(View):
     def get(self, request):
         return render(request, 'main/contact.html')
 
-# class SearchResulPageView(View):
-#     def get(self, request):
-#         search = request.GET.get('search')
-#         if not search:
-#             fruits = Fruit.objects.all()
-#             context = {
-#                 'fruits': fruits, 'search': search
-#             }
-#             return render(request, 'main/404.html', context)
-#         else:
-#             fruits = Fruit.objects.filter(title__icontains=search)
-#             if fruits:
-#                 context = {
-#                     'fruits': fruits, 'search': search
-#                 }
-#                 return render(request, 'main/search_result.html', context)
-#             else:
-#                 return render(request, 'main/404.html')
-#
-#     def get_object(self, fruit_id):
-#         return get_object_or_404(Fruit, id=fruit_id)
 
 
 class SearchResulPageView(View):
     def get(self, request):
         search = request.GET.get('search')
-        fruits = Fruit.objects.all()  # Default queryset
+        fruit = Fruit.objects.all()  # Default queryset
 
         if search:  # If search query exists
-            fruits = Fruit.objects.filter(title__icontains=search)
+            fruit = Fruit.objects.filter(title__icontains=search)
 
-        context = {'fruits': fruits, 'search': search}
+        context = {'fruit': fruit, 'search': search}
         return render(request, 'main/search_result.html', context)
 
 
 
 
-# class SearchResultsList(ListView):
-#     model = Fruit
-#     template_name = 'main/search_result.html'
-#     context_object_name = 'all_fruit'
-#
-#     def get_queryset(self):
-#         query = self.request.GET.get('s')
-#         return Fruit.objects.filter(
-#             Q(title__icontains=query) | Q(body__icontains=query)
-#         )
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['categories'] = Fruit.objects.all()
-#         return context
-
-
-    # def get(self, request):
-    #     search = request.GET.get('search')
-    #     if not search:
-    #         fruits = Fruit.objects.all()
-    #         context = {'fruits': fruits}
-    #         return render(request, 'main/search.html', context)
-    #     else:
-    #         fruits = Fruit.objects.all(title__icontains=search)
-    #         if fruits:
-    #             context = {'fruits': fruits}
-    #             return render(request, 'main/search.html', context)
-    #         else:
-    #             context = {'fruits': fruits}
-    #             return render(request, 'main/404.html')
