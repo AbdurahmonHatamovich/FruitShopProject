8e19f495fdcd5f2fa32d28d6f2b8c06231442e44
ABU <muruvat@gmail.com>
2024-05-08 17:18:55 +0500

    allowed hosts

--- products/settings.py
+++ products/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["146.190.145.149"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
